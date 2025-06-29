b1bb78fc58872147a6824db490791a7c339c4085
mdsamiullah <149084547+mdsamiullah-in@users.noreply.github.com>
2025-06-29 18:59:40 +0530

    Update fetcher.js

--- util/fetcher.js
+++ util/fetcher.js
@@ -1,14 +1,19 @@
 import http from "./http";
 
-const fetcher = async (url)=>{
-    try {
-        const {data} = await http.get(url)
-        return data
+const fetcher = async (url) => {
+  try {
+    const { data } = await http.get(url);
+    return data;
+  } catch (err) {
+    // Agar backend se response mila hai
+    if (err.response) {
+      throw new Error(err.response.data?.message || "Server Error");
+    } else if (err.request) {
+      throw new Error("No response from server");
+    } else {
+      throw new Error(err.message || "Request failed");
     }
-    catch(err)
-    {
-        throw new Error(err)
-    }
-}
+  }
+};
 
-export default fetcher
+export default fetcher;
